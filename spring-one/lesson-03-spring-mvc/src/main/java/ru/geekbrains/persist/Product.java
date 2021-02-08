66c0de3c17a398eae260491dcc1750d8765e1b7c
sonyawender <sonya.wender@gmail.com>
2021-02-08 18:13:57 +0300

    Homework Lesson 3
    Homework Lesson 3

---
+++ spring-one/lesson-03-spring-mvc/src/main/java/ru/geekbrains/persist/Product.java
@@ -0,0 +1,47 @@
+package ru.geekbrains.persist;
+
+public class Product {
+    private long id;
+    private String name;
+    private String description;
+    private int price;
+
+    public Product() {
+    }
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}

fffbad37cf94b2c30eb8c70009d8615710a9b3b6
sonyawender <sonya.wender@gmail.com>
2021-02-12 18:01:32 +0300

    Homework Lesson 4
    Homework Lesson 4

--- spring-one/lesson-04-spring-boot/src/main/java/ru/geekbrains/persist/Product.java
+++ spring-one/lesson-04-spring-boot/src/main/java/ru/geekbrains/persist/Product.java
@@ -1,7 +1,5 @@
 package ru.geekbrains.persist;
 
-import org.springframework.lang.NonNull;
-
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
